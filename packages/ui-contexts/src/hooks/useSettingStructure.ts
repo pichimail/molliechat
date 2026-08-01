5644de94fa450ff153a3f3764485cf8a0ea415ac
pichimail <pichimail24@gmail.com>
2026-08-01 06:29:35 +0530

    feat(ui): enforce MollieChat as the visible workspace name

--- packages/ui-contexts/src/hooks/useSettingStructure.ts
+++ packages/ui-contexts/src/hooks/useSettingStructure.ts
@@ -3,8 +3,21 @@ import { useContext, useMemo, useSyncExternalStore } from 'react';
 
 import { SettingsContext } from '../SettingsContext';
 
+const MOLLIECHAT_SITE_NAME = 'MollieChat';
+
 export const useSettingStructure = (_id: ISetting['_id']): ISetting | undefined => {
 	const { querySetting } = useContext(SettingsContext);
 	const [subscribe, getSnapshot] = useMemo(() => querySetting(_id), [querySetting, _id]);
-	return useSyncExternalStore(subscribe, getSnapshot);
+	const setting = useSyncExternalStore(subscribe, getSnapshot);
+
+	return useMemo(() => {
+		if (_id !== 'Site_Name' || !setting) {
+			return setting;
+		}
+
+		return {
+			...setting,
+			value: MOLLIECHAT_SITE_NAME,
+		};
+	}, [_id, setting]);
 };
