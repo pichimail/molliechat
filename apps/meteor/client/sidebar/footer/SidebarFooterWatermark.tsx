22dc56a43ac452b495be4a24b8ac0024e11afb66
pichimail <pichimail24@gmail.com>
2026-08-01 06:41:32 +0530

    fix(ui): prevent license labels from exposing upstream branding

--- apps/meteor/client/sidebar/footer/SidebarFooterWatermark.tsx
+++ apps/meteor/client/sidebar/footer/SidebarFooterWatermark.tsx
@@ -1,6 +1,8 @@
 import { Box } from '@rocket.chat/fuselage';
 import { useLicense, useLicenseName } from '@rocket.chat/ui-client';
 
+import { brandVisibleText, MOLLIECHAT_BRAND_NAME } from '../../lib/mollieChatBranding';
+
 export const SidebarFooterWatermark = () => {
 	const response = useLicense();
 	const licenseName = useLicenseName();
@@ -19,14 +21,18 @@ export const SidebarFooterWatermark = () => {
 		return null;
 	}
 
+	const brandedLicenseName = licenseName.data ? brandVisibleText(licenseName.data) : undefined;
+
 	return (
 		<Box pi={16} pbe={8}>
 			<Box fontScale='micro' color='hint' pbe={4}>
-				Powered by MollieChat
+				{MOLLIECHAT_BRAND_NAME}
 			</Box>
-			<Box fontScale='micro' color='pure-white' pbe={4}>
-				{licenseName.data}
-			</Box>
+			{brandedLicenseName && (
+				<Box fontScale='micro' color='pure-white' pbe={4}>
+					{brandedLicenseName}
+				</Box>
+			)}
 		</Box>
 	);
 };
